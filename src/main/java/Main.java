95ed04e181342ae3da0662ee64aa09e0053be1fb
anna <annaaplavina@gmail.com>
2024-07-19 18:45:03 +0100

    chtcking url

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.StringTokenizer;
 
 public class Main {
   public static void main(String[] args) {
@@ -17,9 +19,20 @@ public class Main {
 
       Socket socket = serverSocket.accept(); // Wait for connection from client.
       System.out.println("accepted new connection");
-      OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream());
-      osw.write("HTTP/1.1 200 OK\r\n\r\n");
-      osw.flush();
+      try (BufferedReader reader =
+              new BufferedReader(new InputStreamReader(socket.getInputStream()));
+          BufferedWriter writer =
+              new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())); ) {
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        String requestType = tokenizer.nextToken();
+        String url = tokenizer.nextToken();
+        if (url.equals("/")) {
+          writer.write("HTTP/1.1 200 OK\r\n\r\n");
+        } else {
+          writer.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+        writer.flush();
+      }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
