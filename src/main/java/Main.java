ad208f453f1439316dcedd2de05779ab0019081c
anna <annaaplavina@gmail.com>
2024-07-19 18:31:42 +0100

    response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,8 +15,11 @@ public class Main {
       // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
 
-      serverSocket.accept(); // Wait for connection from client.
+      Socket socket = serverSocket.accept(); // Wait for connection from client.
       System.out.println("accepted new connection");
+      OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream());
+      osw.write("HTTP/1.1 200 OK\r\n\r\n");
+      osw.flush();
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
