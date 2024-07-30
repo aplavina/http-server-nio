240eba6a1d91f0c78c7fe39d6d731239f11f70dc
anna <annaaplavina@gmail.com>
2024-07-30 10:23:13 +0100

    fix

--- src/main/java/SimpleHandler.java
+++ src/main/java/SimpleHandler.java
@@ -4,12 +4,28 @@ public class SimpleHandler implements HttpRequestHandler {
   public String handle(String requestStr) {
     HttpRequest request = new HttpRequest(requestStr);
     HttpResponse httpResponse = new HttpResponse();
-    httpResponse.setStatus(200);
-    httpResponse.setStatusDescr("OK");
-    String body = request.getHeaders().get("User-Agent");
-    httpResponse.setBody(body);
-    httpResponse.addHeader("Content-Type", "text/plain");
-    httpResponse.addHeader("Content-Length", "" + body.length());
+
+    if (request.getUrl().equals("/")) {
+      httpResponse.setStatus(200);
+      httpResponse.setStatusDescr("OK");
+    } else if (request.getUrl().equals("/user-agent")) {
+      httpResponse.setStatus(200);
+      httpResponse.setStatusDescr("OK");
+      String body = request.getHeaders().get("User-Agent");
+      httpResponse.setBody(body);
+      httpResponse.addHeader("Content-Type", "text/plain");
+      httpResponse.addHeader("Content-Length", "" + body.length());
+    } else if (request.getUrl().startsWith("/echo/")) {
+      httpResponse.setStatus(200);
+      httpResponse.setStatusDescr("OK");
+      final String body = request.getUrl().split("/echo/")[1];
+      httpResponse.setBody(body);
+      httpResponse.addHeader("Content-Type", "text/plain");
+      httpResponse.addHeader("Content-Length", "" + body.length());
+    } else {
+      httpResponse.setStatus(404);
+      httpResponse.setStatusDescr("Not Found");
+    }
     return httpResponse.getResponseString();
   }
 }
